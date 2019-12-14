832153c92f2b3b096dd25f17ccb64142b2b74d36
Seika139 <tcoanzu@gmail.com>
2019-12-14 16:35:27 +0900

    DB接続ができた!!!!

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,20 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
     public function index(Request $request){
-        return view('hello.index', ['msg'=>'fill out the form:']);
-    }
-
-    public function post(Request $request){
-      $validate_rule = [
-        'name' => 'required',
-        'mail' => 'email',
-        'age' => 'numeric|between:0,150',
-      ];
-      $this->validate($request,$validate_rule);
-      return view('hello.index', ['msg'=>'The form was inputed successfully.']);
+        $items = DB::select('select * from articles');
+        return view('hello.index', ['items' => $items]);
     }
 }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,24 +9,15 @@
 @endsection
 
 @section('content')
-  <p>{{$msg}}</p>
-  @if (count($errors) > 0)
-  <div>
-    <ul>
-      @foreach ($errors->all() as $error)
-        <li>{{$error}}</li>
-      @endforeach
-    </ul>
-  </div>
-  @endif
   <table>
-    <form action="/hello" method="post">
-      {{csrf_field()}}
-      <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
-      <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
-      <tr><th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
-      <tr><th></th><td><input type="submit" value="send"></td></tr>
-    </form>
+    <tr><th>Id</th><th>WriterId</th><th>Title</th></tr>
+    @foreach($items as $item)
+      <tr>
+        <td>{{$item->id}}</td>
+        <td>{{$item->writer_id}}</td>
+        <td>{{$item->title}}</td>
+      </tr>
+    @endforeach
   </table>
 @endsection
 
