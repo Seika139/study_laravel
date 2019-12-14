c070898ae96070d76e78ce26711445142bb5730c
Seika139 <tcoanzu@gmail.com>
2019-12-14 15:42:58 +0900

    127ページ : バリデーションはvue.jsでやるのでさっさとDBにいきましょう

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,34 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-global $head,$style,$body,$end;
-
-$head = '<html><head>';
-$style = <<<EOF
-<style>
-  body {font-size:100pt; text-align:center; color:#eee;
-  margin:-40px 0px -50px 0px; }
-</style>
-EOF;
-
-$body = '</head><body>';
-$end = '</body></head>';
-
-function tag($tag,$txt){
-  return "<{$tag}>" . $txt . "</{$tag}>";
-}
-
 class HelloController extends Controller
 {
     public function index(Request $request){
-        return view('hello.index');
+        return view('hello.index', ['msg'=>'fill out the form:']);
     }
 
     public function post(Request $request){
-      $msg = $request->msg;
-      $data = [
-        'msg'=>'Hello '.$msg . '!!',
+      $validate_rule = [
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'numeric|between:0,150',
       ];
-      return view('hello.index',$data);
+      $this->validate($request,$validate_rule);
+      return view('hello.index', ['msg'=>'The form was inputed successfully.']);
     }
 }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,9 +9,25 @@
 @endsection
 
 @section('content')
-  <p>This is a main part of this application.</p>
-  <p>This is a link to <middleware>google.com</middleware>.</p>
-  <p>This is a link to <middleware>yahoo.co.jp</middleware>.</p>
+  <p>{{$msg}}</p>
+  @if (count($errors) > 0)
+  <div>
+    <ul>
+      @foreach ($errors->all() as $error)
+        <li>{{$error}}</li>
+      @endforeach
+    </ul>
+  </div>
+  @endif
+  <table>
+    <form action="/hello" method="post">
+      {{csrf_field()}}
+      <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+      <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
+      <tr><th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
+      <tr><th></th><td><input type="submit" value="send"></td></tr>
+    </form>
+  </table>
 @endsection
 
 @section('footer')
